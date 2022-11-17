84a4ca9ed22c420bb2558d56c5ee1d24ad3b65dd
David Rodriguez <roblespabon@gmail.com>
2022-11-17 09:11:37 -0600

    Elementos en carpeta de Nóminas.

--- ProyectoFinal/Formularios/Nominas/FrmHistorialNominas.cs
+++ ProyectoFinal/Formularios/Nominas/FrmHistorialNominas.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaNegocio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,7 @@ namespace ProyectoFinal.Formularios.Nominas
 {
     public partial class FrmHistorialNominas : Form
     {
+        DatosNominas objeto = new DatosNominas();
         public FrmHistorialNominas()
         {
             InitializeComponent();
