37b675661275bd3acfa6332293189b0e92ba0932
Joshua Aburto <obedjoshuaburtolopez@gmail.com>
2022-11-22 03:01:50 -0600

    Mejorando el módulo sesion

--- ProyectoFinal/Formularios/Sesiones/FrmModificarSesion.Designer.cs
+++ ProyectoFinal/Formularios/Sesiones/FrmModificarSesion.Designer.cs
@@ -28,11 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmModificarSesion));
             this.panelContenedor = new System.Windows.Forms.Panel();
-            this.paneldatos = new System.Windows.Forms.Panel();
             this.btnMenu = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.paneldatos = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
             this.cmbxAño = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
@@ -60,22 +62,6 @@
             this.panelContenedor.Size = new System.Drawing.Size(939, 643);
             this.panelContenedor.TabIndex = 0;
             // 
-            // paneldatos
-            // 
-            this.paneldatos.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.paneldatos.Controls.Add(this.label5);
-            this.paneldatos.Controls.Add(this.cmbxAño);
-            this.paneldatos.Controls.Add(this.label4);
-            this.paneldatos.Controls.Add(this.label3);
-            this.paneldatos.Controls.Add(this.cmbxMes);
-            this.paneldatos.Controls.Add(this.lblTotal);
-            this.paneldatos.Controls.Add(this.label2);
-            this.paneldatos.Controls.Add(this.dataGridView1);
-            this.paneldatos.Location = new System.Drawing.Point(27, 51);
-            this.paneldatos.Name = "paneldatos";
-            this.paneldatos.Size = new System.Drawing.Size(859, 522);
-            this.paneldatos.TabIndex = 1;
-            // 
             // btnMenu
             // 
             this.btnMenu.BackColor = System.Drawing.SystemColors.Control;
@@ -99,6 +85,22 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Lista de Sesiones";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // paneldatos
+            // 
+            this.paneldatos.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.paneldatos.Controls.Add(this.label5);
+            this.paneldatos.Controls.Add(this.cmbxAño);
+            this.paneldatos.Controls.Add(this.label4);
+            this.paneldatos.Controls.Add(this.label3);
+            this.paneldatos.Controls.Add(this.cmbxMes);
+            this.paneldatos.Controls.Add(this.lblTotal);
+            this.paneldatos.Controls.Add(this.label2);
+            this.paneldatos.Controls.Add(this.dataGridView1);
+            this.paneldatos.Location = new System.Drawing.Point(27, 51);
+            this.paneldatos.Name = "paneldatos";
+            this.paneldatos.Size = new System.Drawing.Size(859, 522);
+            this.paneldatos.TabIndex = 1;
             // 
             // label5
             // 
@@ -193,12 +195,24 @@
             // 
             // dataGridView1
             // 
-            this.dataGridView1.AllowDrop = true;
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.MistyRose;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Sienna;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.Location = new System.Drawing.Point(16, 58);
-            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
